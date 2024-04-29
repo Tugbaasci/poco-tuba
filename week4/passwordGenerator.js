d4839adab617371c0c42e3c1bc978478289adf65
tuba <tugba.asci1419@gmail.com>
2024-04-29 11:39:10 +0200

    password generator done

--- week4/passwordGenerator.js
+++ week4/passwordGenerator.js
@@ -1,12 +1,25 @@
+function generatePassword(){
 const specialChars = ['@','#','$','%','&','-','_'];
 const numbers = "0123456789";
-const letters ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const letters ="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let password = "";
+let length = 8;
 const randomSpecialIndex = Math.floor(Math.random()*specialChars.length);
 const randomSpecial = specialChars[randomSpecialIndex];
-console.log(randomSpecial);
+password +=randomSpecial;
+
 const randomNumbersIndex = Math.floor(Math.random()*numbers.length);
 const randomNumber = numbers[randomNumbersIndex];
-console.log(randomNumber);
-const randomLettersIndex = Math.floor(Math.random()*letters.length);
-const randomLetter = letters[randomLettersIndex];
-console.log(randomLetter);
+password += randomNumber;
+
+let remainingLength =  length - password.length;
+for (let i = 0; i < remainingLength; i++) {
+    password += letters.charAt(Math.floor(Math.random()*letters.length))
+};
+password = password.split("").sort(function(){
+    return 0.5 - Math.random();
+}).join('');
+return password;
+
+}
+console.log(generatePassword());
